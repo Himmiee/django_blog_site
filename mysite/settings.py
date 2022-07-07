88bdd83b226cbac0041dc6e3bb2ab08cae4cbfe2
Haliyah Tijani <haliyya460@gmail.com>
2022-07-07 12:45:40 +0100

    add database postgress

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-from msilib.schema import Environment
 import os
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
